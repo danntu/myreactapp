329c3022071f462948fc6923fb2b18cc41a7afb1
Myrzakanov Daniyar <dan_ntu@mail.ru>
2018-12-31 00:37:01 +0600

    Введение, компонентный подход https://learn.javascript.ru/screencast/react#react-components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from  'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Helloworld(){
+    return (
+        <div>
+            <h1> Hello World Danik</h1>
+        </div>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Helloworld/>, document.getElementById('root'))
+render(<Helloworld/>, document.getElementById('root2'))
