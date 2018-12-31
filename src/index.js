b34cea1093d29827793f85a35dd21491210a413d
Myrzakanov Daniyar <dan_ntu@mail.ru>
2018-12-31 18:51:09 +0600

    Настройка окружения, create-react-app https://learn.javascript.ru/screencast/react#react-environment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 import React from  'react'
+import RenderDOM from 'react-dom'
 import {render} from 'react-dom'
 
 function Helloworld(){
     return (
         <div>
-            <h1> Hello World Danik</h1>
+            <h1>(Первый пример) Hello World Danik</h1>
         </div>
     )
 }
 
-render(<Helloworld/>, document.getElementById('root'))
-render(<Helloworld/>, document.getElementById('root2'))
+function SecondHello(){
+    return (
+        <div>
+            <h2>(Второй пример) Привет второй раз</h2>
+        </div>
+    )
+}
+
+function ThirdHello(){
+    return (
+      <div>
+          <h3>(Третий пример) Привет третий раз</h3>
+      </div>
+    )
+}
+
+
+RenderDOM.render(<Helloworld/>, document.getElementById('root1'))
+render(<SecondHello/>, document.getElementById('root2'))
+render(<ThirdHello/>, document.getElementById('root3'))
