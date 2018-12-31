d5a76d7efb29d66efc7df856750f12ee2ab995a1
Myrzakanov Daniyar <dan_ntu@mail.ru>
2018-12-31 19:14:11 +0600

    JSX - синтаксис для разметки в React.js https://learn.javascript.ru/screencast/react#react-jsx

--- src/index.js
+++ src/index.js
@@ -26,7 +26,32 @@ function ThirdHello(){
     )
 }
 
+function Article(){
+    const const_body = <section>const body</section>
+    return (
+        <div className="hello" style={{color:'red'}}>
+            <h2>title</h2>
+            <section>body</section>
+            {const_body}
+            <h3>
+                creation date: {(new Date).toDateString()}
+            </h3>
+        </div>
+    )
+}
+
+function App(){
+    return (
+        <div>
+            <h1>App Name</h1>
+            <Article></Article>
+        </div>
+    )
+}
+
 
 RenderDOM.render(<Helloworld/>, document.getElementById('root1'))
 render(<SecondHello/>, document.getElementById('root2'))
 render(<ThirdHello/>, document.getElementById('root3'))
+
+RenderDOM.render(<App/>, document.getElementById('root4'))
