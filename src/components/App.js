71dd1680c537c2f886238f0e0b1cc458c7129dbf
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-01-02 16:05:41 +0600

    Оформление компонентов, работа с CSS https://learn.javascript.ru/screencast/react#react-css

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Article from './Article'
 import articles from '../fixtures'
 import ArticleList from './ArticleList'
+import 'bootstrap/dist/css/bootstrap.css'
 
 
 // function App(){
@@ -16,8 +17,10 @@ import ArticleList from './ArticleList'
 
 function App(){
     return (
-        <div>
-            <h1>App Name</h1>
+        <div className="container">
+            <div className="jumbotron">
+            <h1 className="display-3">App Name</h1>
+            </div>
             <ArticleList articles={articles} />
         </div>
     )
--- src/components/Article.js
+++ src/components/Article.js
@@ -16,22 +16,26 @@ class Article extends Component{
 
     render() {
         const {article} = this.props
-
+        const style ={width:'50%'}
 
         console.log("=====", this.props)
 
-        const const_body = this.state.isOpen && <section>{article.text}</section>
+        const const_body = this.state.isOpen && <section className="card-text">{article.text}</section>
         return (
-            <div>
-                <h2>{article.title}
-                    <button onClick={this.handleClick}>
-                        {this.state.isOpen ? 'close' : 'open' }
-                    </button>
-                </h2>
-                {const_body}
-                <h3>
-                    Article creation date: {(new Date(article.date)).toDateString()}
-                </h3>
+            <div className="card mx-auto" style={style}>
+                <div className="card-header">
+                    <h2>{article.title}
+                        <button onClick={this.handleClick} className="btn btn-primary btn-lg float-right">
+                            {this.state.isOpen ? 'close' : 'open' }
+                        </button>
+                    </h2>
+                </div>
+                <div className="card-body">
+                    <h6 className="card-subtitle text-muted">
+                        Article creation date: {(new Date(article.date)).toDateString()}
+                    </h6>
+                    {const_body}
+                </div>
             </div>
         )
     }
