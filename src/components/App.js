39993fcb4a0f6d075b5a3bcd545614bdc7083bc3
Myrzakanov Daniyar <dan_ntu@mail.ru>
2019-01-02 14:32:32 +0600

    Отображение массивов, смысл аттрибута key https://learn.javascript.ru/screencast/react#react-lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Article from './Article'
 import articles from '../fixtures'
+import ArticleList from './ArticleList'
+
+
+// function App(){
+//     return (
+//         <div>
+//             <h1>App Name</h1>
+//             <Article article={articles[0]} foo="bar" flag></Article>
+//         </div>
+//     )
+// }
+
 
 function App(){
     return (
         <div>
             <h1>App Name</h1>
-            <Article article={articles[0]} foo="bar" flag></Article>
+            <ArticleList articles={articles} />
         </div>
     )
 }
